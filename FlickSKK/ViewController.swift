15f19aeae33d0ea05a088df4672d2ea1c0714bc3
banjun <banjun@gmail.com>
2014-09-27 01:26:57 +0900

    Initial Commit

---
+++ FlickSKK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickSKK
+//
+//  Created by BAN Jun on 2014/09/27.
+//  Copyright (c) 2014年 BAN Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
