6810416ba47a33ebf9cf3ef09b23f5413db561dd
mzp <mzpppp@gmail.com>
2014-10-06 23:33:14 +0900

    add simple/sample spec refs #9

---
+++ FlickSKKTests/UtilitiesSpec.swift
@@ -0,0 +1,23 @@
+//
+//  UtilitiesSpec.swift
+//  FlickSKK
+//
+//  Created by mzp on 2014/10/06.
+//  Copyright (c) 2014年 BAN Jun. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class UtilitiesSpec: QuickSpec {
+    override func spec() {
+        describe("String") {
+            context("ひらがな to カタカナ") {
+                it("is convert あ行") {
+                    let result = "あいうえお".conv(.Hirakana, to: .Katakana)
+                    expect(result).to(equal("アイウエオ"))
+                }
+            }
+        }
+    }
+}
