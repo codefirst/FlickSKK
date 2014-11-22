d011f8db0c6a18bfbf91e00088bf477b5e1236d8
banjun <banjun@gmail.com>
2014-11-23 08:53:41 +0900

    add scripts for verifying and uploading iTunes Connect metadata

---
+++ iTunesConnect/verify.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# usage: $0 iTunesConnectAccount(email) SKU
+iTMSTransporter="$(xcode-select -p)/../Applications/Application Loader.app/Contents/MacOS/itms/bin/iTMSTransporter"
+"$iTMSTransporter" -m verify -u $1 -f $2.itmsp
