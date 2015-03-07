6a2e0096e74e93618053378781952c29abba05d7
MIZUNO Hiroki <mzpppp@gmail.com>
2015-03-07 23:25:43 +0900

    :abcd: 学習辞書が更新されている場合は、辞書を再ロードするようにした

--- FlickSKKKeyboard/DictionaryCache.swift
+++ FlickSKKKeyboard/DictionaryCache.swift
@@ -1,5 +1,5 @@
 private var kGlobalDictionary: SKKDictionary?
-private var kLoadedTime : NSDate? = nil
+private var kLoadedTime : [String:NSDate] = [:]
 
 // 辞書のロードには時間がかかるので、一度ロードした結果をキャッシュする
 // グローバル変数にいれておけば、次回起動時にも残っている(ことがある)
@@ -7,15 +7,22 @@ class DictionaryCache {
     class func load() -> SKKDictionary? {
         let userDict = SKKUserDictionaryFile.defaultUserDictionaryPath()
         let learnDict = SKKUserDictionaryFile.defaultLearnDictionaryPath()
-        let mtime = getModifiedTime(userDict)
 
-        if kGlobalDictionary == nil || kLoadedTime != mtime {
+        // 辞書がロードされていない or ユーザ辞書・学習辞書が更新されている場合は再ロードする
+        if kGlobalDictionary == nil || isUpdated(userDict) || isUpdated(learnDict) {
             let dict = NSBundle.mainBundle().pathForResource("skk", ofType: "jisyo")
             kGlobalDictionary = SKKDictionary(userDict: userDict, learnDict: learnDict, dicts: [dict!])
-            kLoadedTime = mtime
         }
 
         return kGlobalDictionary
+    }
+
+    private class func isUpdated(path: String) -> Bool {
+        return kLoadedTime[path] != getModifiedTime(path)
+    }
+
+    private class func cache(path: String) {
+        kLoadedTime[path] = getModifiedTime(path)
     }
 
     private class func getModifiedTime(path: String) -> NSDate? {
