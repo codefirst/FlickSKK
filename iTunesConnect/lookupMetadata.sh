263face8f70d2170c458dd7693f7ff666c551ba5
banjun <banjun@gmail.com>
2015-04-19 21:20:47 +0900

    lookup metadata for FlickSKK 1.3 and Xcode 6.3 refs #115

--- iTunesConnect/lookupMetadata.sh
+++ iTunesConnect/lookupMetadata.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # usage: $0 iTunesConnectAccount(email) SKU
-iTMSTransporter="$(xcode-select -p)/../Applications/Application Loader.app/Contents/MacOS/itms/bin/iTMSTransporter"
+iTMSTransporter="$(xcode-select -p)/../Applications/Application Loader.app/Contents/itms/bin/iTMSTransporter"
 "$iTMSTransporter" -m lookupMetadata -u $1 -vendor_id $2 -destination .
--- iTunesConnect/verify.sh
+++ iTunesConnect/verify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # usage: $0 iTunesConnectAccount(email) SKU
-iTMSTransporter="$(xcode-select -p)/../Applications/Application Loader.app/Contents/MacOS/itms/bin/iTMSTransporter"
+iTMSTransporter="$(xcode-select -p)/../Applications/Application Loader.app/Contents/itms/bin/iTMSTransporter"
 "$iTMSTransporter" -m verify -u $1 -f $2.itmsp
