ba447ab5976d5bc7ca1ea6155043f4611407dc56
banjun <banjun@gmail.com>
2021-09-15 05:33:31 +0900

    add ci scripts for Xcode Cloud

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Set the -e flag to stop running the script in case a command returns
+# a non-zero exit code.
+set -e
+set -x
+
+bundle install
+if [[ ! -e .cocoapods_appgroup ]]; then
+    bundle exec pod app-group org.codefirst.FlickSKK
+fi
+bundle exec pod install
